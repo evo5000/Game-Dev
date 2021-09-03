17295b8ae7ae628193793c6b01c7e19f2481bb91
will <will.j970@stu.nebo.edu>
2021-09-03 14:19:01 -0600

    moveeee

--- prtotype 1 tank sim/Assets/Scripts/PlayerControler.cs
+++ prtotype 1 tank sim/Assets/Scripts/PlayerControler.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     public float Speed;
+
     public float TurnSpeed;
+
+    public float hInput;
+
+    public float vInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +21,12 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
         //transform.position(Vector3(0,0,0));
-        transform.Translate(Vector3.forward * Speed);
+        //dosnt move tank forword
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime * vInput);
+        transform.Rotate(Vector3.up, TurnSpeed * hInput * Time.deltaTime );
     }
 }
