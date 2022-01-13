50f094dab19c57ed69c8bc5f22ce9d02534f06da
Will Jenkins <will.j970@stu.nebo.edu>
2022-01-13 00:52:15 -0700

    roll the ball game thing
    the unity stuff

---
+++ Roll the ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+ //it do the stuff dont toch it stops working if you touch bad no no code
+	private Vector3 offset;
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+		void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll the ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	//declare the stuff
+	public Text winText;
+	public float speed;
+	private Rigidbody rb;
+	public Text countText;
+	private int count;
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		// ok soooooo ummmm we need this but every time i put you back in everything dies again sooooo ima just leave this here XD
+		//wintext.text = "";
+	}
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{	
+			other.gameObject.SetActive (false);
+
+
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString (); 
+		if (count >= 14) 
+		{
+			winText.text = "win.exe";
+		}
+
+	}
+
+}
